af1b8c6825b1daebd32744adfcc3cba6fbf58ec5
shihori711 <thremin711@gmail.com>
2020-02-06 21:53:52 +0900

    修正

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,27 +1,27 @@
-$(function() {
-  $("#user-search-field").on("keyup", function() {
-    let input = $("#user-search-field").val();
-    console.log(input);
-  });
-});
+// $(function() {
+//   $("#user-search-field").on("keyup", function() {
+//     let input = $("#user-search-field").val();
+//     console.log(input);
+//   });
+// });
 
-$(function() {
-  $("#user-search-field").on("keyup", function() {
-    let input = $("#user-search-field").val();
-    $.ajax({
-      type: "GET",
-      url: "/users",
-      data: { keyword: input },
-      dataType: "json"
-    })
-      .done(function(users) {
-        console.log("成功です");
-      })
-      .fail(function() {
-        console.log("失敗です");
-      });
-  });
-});
+// $(function() {
+//   $("#user-search-field").on("keyup", function() {
+//     let input = $("#user-search-field").val();
+//     $.ajax({
+//       type: "GET",
+//       url: "/users",
+//       data: { keyword: input },
+//       dataType: "json"
+//     })
+//       .done(function(users) {
+//         console.log("成功です");
+//       })
+//       .fail(function() {
+//         console.log("失敗です");
+//       });
+//   });
+// });
 
 $(function() {
   function addUser(user) {
@@ -42,6 +42,19 @@ $(function() {
     `;
     $("#user-search-result").append(html);
   }
+  function addDeleteUser(name, id) {
+    let html = `
+    <div class="chat-group-user clearfix" id="${id}">
+      <p class="chat-group-user__name">${name}</p>
+      <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
+    </div>`;
+    $(".js-add-user").append(html);
+  }
+  function addMember(userId) {
+    let html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
+    $(`#${userId}`).append(html);
+  }
+
   $("#user-search-field").on("keyup", function() {
     let input = $("#user-search-field").val();
     $.ajax({
@@ -57,7 +70,7 @@ $(function() {
           users.forEach(function(user) {
             addUser(user);
           });
-        } else if (input.length == 0) {
+        } else if (input.length === 0) {
           return false;
         } else {
           addNoUser();
@@ -66,5 +79,20 @@ $(function() {
       .fail(function() {
         alert("通信エラーです。ユーザーが表示できません。");
       });
+    });
+      $(document).on("click", ".chat-group-user__btn--add", function() {
+        console.log
+        const userName = $(this).attr("data-user-name");
+        const userId = $(this).attr("data-user-id");
+        $(this)
+          .parent()
+          .remove();
+        addDeleteUser(userName, userId);
+        addMember(userId);
+      });
+      $(document).on("click", ".chat-group-user__btn--remove", function() {
+        $(this)
+          .parent()
+          .remove();
   });
 });
